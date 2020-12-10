a9d4bc8ea4d5fcaa757bc5a46c31cfa69216c984
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-12-10 18:35:06 +0000

    track component fatto

--- first-routed-app/src/app/app-routing.module.ts
+++ first-routed-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
+import { TrackComponent } from './track/track.component';
 
 //Si scrivono le route
 const routes: Routes = [
   { path: 'about', component: AboutComponent},
-  { path: 'search', component: SearchComponent}
+  { path: 'search', component: SearchComponent},
+  { path: 'tracks/:id', component: TrackComponent}
 ];
 
 @NgModule({
--- first-routed-app/src/app/track/track.component.ts
+++ first-routed-app/src/app/track/track.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ParamMap, ActivatedRoute, Router } from '@angular/router';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-track',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./track.component.css']
 })
 export class TrackComponent implements OnInit {
+  //Osserva gli eventi sulla route tracks, restituisce la ParamMap che contiene tutti i
+  //parametri passati all’url
+  routeObs : Observable<ParamMap>;
+  track : any; //Salvo la traccia selezionata
 
-  constructor() { }
+  /*Usiamo la dependency injection per farci mandare i moduli del routing e dello SpotifyService*/
+  constructor(
+    private route : ActivatedRoute,
+    private router : Router,
+    private service : SpotifyService
+  ) { }
 
   ngOnInit(): void {
+    //Si ottiene observable che notifica le informazioni sulla route attiva
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
   }
+
+  //Ogni volta che viene invocata la route tracks/:id, l'observable richiama questo metodo
+  getRouterParam = (params: ParamMap) =>
+  {
+    let trackId = params.get('id');    //id della ParamMap
+    console.log(trackId);
+    //this.service.getTrack()
+  }
+
+  back()
+  {/*DA FINIRE*/}
 
 }
